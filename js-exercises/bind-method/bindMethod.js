5b65c376e213211c2bd6c79b00e3f559364f4678
Neeraj Yadav <ny29599@gmail.com>
2021-02-13 12:09:33 +0530

    solution-bind-method

--- js-exercises/bind-method/bindMethod.js
+++ js-exercises/bind-method/bindMethod.js
@@ -1,6 +1,8 @@
 // You can change the `args`
-function bind(...args) {
-  return args;
+function bind(fn, obj) {
+  return function hardBind(...args) {
+    return fn.apply(obj, args);
+  };
 }
 
 module.exports = { bind };
